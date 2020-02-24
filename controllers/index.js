caf32d8e8245e696016b639cd088b35fb143a5ae
MayPhueKyawSoe <mayphuekyawsoe123@gmail.com>
2020-02-24 10:45:06 +0630

    conflict controller/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,9 @@
 const employee = require('./employee');
-<<<<<<< HEAD
 const notice = require('./notice');
-module.exports = {
-    employee, notice
-=======
 const role = require('./role');
 const permission = require('./permission');
 const rolepermission = require('./rolepermission');
 
 module.exports = {
-    employee,role,permission,rolepermission
->>>>>>> 51e2e6a50789fd9abe7d152ef7ec553f7c00096c
+    employee, notice,role,permission,rolepermission
 }
