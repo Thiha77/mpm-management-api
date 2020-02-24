3bfe6265a2272724e01cc15daf47dd8d275e412b
mayzin_mmp <mayzin@management-partners.co.jp>
2020-02-24 10:44:34 +0630

    controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,10 @@
 const employee = require('./employee');
-<<<<<<< HEAD
+const user = require('./user');
 const notice = require('./notice');
-module.exports = {
-    employee, notice
-=======
 const role = require('./role');
 const permission = require('./permission');
 const rolepermission = require('./rolepermission');
 
 module.exports = {
-    employee,role,permission,rolepermission
->>>>>>> 51e2e6a50789fd9abe7d152ef7ec553f7c00096c
+    employee, notice , user , role, permission, rolepermission
 }
