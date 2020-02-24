2845987074aed2a1033ea7adc2e120877d4adf71
MayPhueKyawSoe <mayphuekyawsoe123@gmail.com>
2020-02-24 10:36:52 +0630

    Employeeapi

--- controllers/employee.js
+++ controllers/employee.js
@@ -21,7 +21,88 @@ const byId = (req, res) => {
         res.send(JSON.stringify(emp));
     })
 }
+const createEmployee =(req,res) => {
+    return Employee.create({
+        name:req.body.name,
+        alias:req.body.alias,
+        phoneNo:req.body.phoneNo,
+        nrcNo:req.body.nrcNo,
+        personalEmail:req.body.personalEmail,
+        officialEmail:req.body.officialEmail,
+        township:req.body.township,
+        city:req.body.city,
+        address:req.body.address,
+        postalCode:req.body.postalCode,
+        dob:new Date(req.body.dob),
+        gender:req.body.gender,
+        position:req.body.position,
+        basicSalary:req.body.basicSalary,
+        nationality:req.body.nationality,
+        race:req.body.race,
+        maritalStatus:req.body.maritalStatus,
+        employeeStatus:req.body.employeeStatus,
+        photo:req.body.photo,
+        userName:req.body.userName,
+        password:req.body.password
+
+    }).then(res => {
+        res.sendStatus(200);
+      })
+      .catch(err => res.send(JSON.stringify(err)));
+    
+}
+const deleteEmployee =(req,res) => {
+    const id = req.body.id;
+   return  Employee.destroy({
+        where: {
+            id: id
+        }
+    }).then(Employee => res.json({
+        error: false,
+        message: "Deleted Success!"
+    })).catch(error => res.json({
+        error: true,
+        error: error
+    }));
+}
+const updateEmployee=(req,res)=>{
+    const id = req.body.id;
+    return Employee.update({  
+        name:req.body.name,
+        alias:req.body.alias,
+        phoneNo:req.body.phoneNo,
+        nrcNo:req.body.nrcNo,
+        personalEmail:req.body.personalEmail,
+        officialEmail:req.body.officialEmail,
+        township:req.body.township,
+        city:req.body.city,
+        address:req.body.address,
+        postalCode:req.body.postalCode,
+        dob:new Date(req.body.dob),
+        gender:req.body.gender,
+        position:req.body.position,
+        basicSalary:req.body.basicSalary,
+        nationality:req.body.nationality,
+        race:req.body.race,
+        maritalStatus:req.body.maritalStatus,
+        employeeStatus:req.body.employeeStatus,
+        photo:req.body.photo,
+        userName:req.body.userName,
+        password:req.body.password
+        },{
+            where: {
+                id: id
+            }               
+        }).then(Attendance => res.json({
+            error: false,
+            message: "update Success!"
+        }))
+        .catch(error => res.json({
+            error: true,
+            error: error
+        }));
+}
 
 module.exports = {
-    all, byId
+    all, byId,createEmployee,deleteEmployee,updateEmployee
 }
