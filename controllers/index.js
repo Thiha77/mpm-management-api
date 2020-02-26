76edce13dd1c46a9c29577c0ea17fa1b5592686d
eaindrahhh <eaindra54321@gmail.com>
2020-02-26 15:59:40 +0630

    attendance

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const employee = require('./employee');
-const user = require('./user');
+// const user = require('./user');
 const notice = require('./notice');
 const role = require('./role');
 const permission = require('./permission');
 const rolepermission = require('./rolepermission');
+const attendance = require('./attendance');
 
 module.exports = {
-    employee, notice , user , role, permission, rolepermission
+    employee, notice , role, permission, rolepermission, attendance
 }
