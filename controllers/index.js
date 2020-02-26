aa9eb7adf520aa2592aed508efb8ba457f43606e
MayPhueKyawSoe <mayphuekyawsoe123@gmail.com>
2020-02-26 16:01:02 +0630

    mergw with master

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,10 @@
 const employee = require('./employee');
-<<<<<<< HEAD
-// const user = require('./user');
-=======
 const user = require('./user');
->>>>>>> 220a01d5aaf0a6251eac33c9cc1f8b98d7ccc6ac
 const notice = require('./notice');
 const role = require('./role');
 const permission = require('./permission');
 const rolepermission = require('./rolepermission');
 
 module.exports = {
-<<<<<<< HEAD
-    employee, notice , role, permission, rolepermission
-=======
     employee, notice , user , role, permission, rolepermission
->>>>>>> 220a01d5aaf0a6251eac33c9cc1f8b98d7ccc6ac
 }
