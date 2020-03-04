693e32a7939d37743d821b49506784e17f4c5c0e
thiha77 <aungthiha91027@gmail.com>
2020-03-04 09:58:59 +0630

    notice modded

--- controllers/notice.js
+++ controllers/notice.js
@@ -1,14 +1,22 @@
 const Notice = require('../models').Notice;
+const Employee = require('../models').Employee;
 
 const all = (req, res) => {
-    return Notice.findAll()
+    return Notice.findAll({
+        include: [
+            {
+                model: Employee,
+                attributes: ['name']
+            }
+        ]
+    })
     .then( (notices) => {
         res.send(JSON.stringify(notices));
     });
 }
 
 const byId = (req, res) => {
-    id = req.params.id;
+    const id = req.params.id;
     return Notice.findAll({
         where: {
             id: id
@@ -16,19 +24,45 @@ const byId = (req, res) => {
     })
     .then( (notices) => {
         res.send(JSON.stringify(notices));
-    })
+    });
 }
 
 const save = (req, res) => {
-    let body = req.body;
+    const body = req.body;
     return Notice.create({
         ...body
     })
     .then( (notice) => {
         res.send(JSON.stringify(notice));
-    })
+    });
+}
+
+const update = (req, res) => {
+    const body = req.body;
+    const id = req.body.id;
+    return Notice.update( {
+        title: body.title,
+        description: body.description,
+        summary: body.summary,
+        employeeId : body.employeeId
+    }, {
+        where: { id: id }
+    }).then ( (rows) => {
+        res.send(JSON.stringify(rows))
+    });
+}
+
+const destory = (req, res) => {
+    let id = req.body.id;
+    return Notice.destroy({
+        where: {
+            id:id
+        }
+    }).then( (notice) => {
+        res.send(JSON.stringify(notice));
+    });
 }
 
 module.exports = {
-    all, byId, save
+    all, byId, save, update, destory
 }
