a7e540ddec1745de4b18bcfe6ceb76c06123f233
phyuzin-95 <phyuzin2095@gmail.com>
2020-03-10 14:29:47 +0630

    Permission and RolePermission Search

--- routes/permission/index.js
+++ routes/permission/index.js
@@ -7,5 +7,6 @@ permissionRouter.post('/create', permissionController.save);
 permissionRouter.post('/update', permissionController.update);
 permissionRouter.post('/delete', permissionController.destory);
 permissionRouter.get('/byRoleId/:roleId', permissionController.byRoleId);
+permissionRouter.get('/search/:text', permissionController.search);
 
 module.exports = permissionRouter;
